0b0e0f10b44eb6ce31e3d3d3b9e51b88715fcfb3
Thuy Dinh Xuan <thuydx@zendgroup.vn>
2014-12-19 22:35:10 +0700

    init project base
    Signed-off-by: Thuy Dinh Xuan <thuydx@zendgroup.vn>

---
+++ config/autoload/layout.module.global.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'layout' => array(
+        'module_layouts' => array(
+            'enable' => true,
+        ),
+        'controller_layouts' => array(
+            'enable' => false,
+        ),
+        'action_layouts' => array(
+            'enable' => false,
+        )
+    ),
+);
---
+++ definition.php
@@ -0,0 +1,38 @@
+<?php
+
+defined('DS')
+|| define('DS', DIRECTORY_SEPARATOR);
+
+define('REQUEST_MICROTIME', microtime(true))
+|| define('REQUEST_MICROTIME', microtime(true));
+
+defined('WEB_ROOT')
+|| define('WEB_ROOT', realpath(dirname(__FILE__)));
+
+// defined('LIBRARY_PATH')
+// || define('LIBRARY_PATH', WEB_ROOT . DS . 'library' . DS);
+
+defined('PUBLIC_PATH')
+|| define('PUBLIC_PATH', WEB_ROOT . DS . 'public' . DS);
+
+defined('ZF2_LIBRARY')
+|| define('ZF2_LIBRARY', realpath((getenv('ZF2_PATH')?getenv('ZF2_PATH'):'vendors\ZF2\library')));
+
+defined('DT2_LIBRARY')
+|| define('DT2_LIBRARY', realpath((getenv('DT2_PATH')?getenv('DT2_PATH'):'vendors\Doctrine2')));
+
+defined('APPLICATION_ENV')
+|| define('APPLICATION_ENV', getenv('APPLICATION_ENV'));
+
+defined('FRONT_TEMPLATES_PATH')
+|| define('FRONT_TEMPLATES_PATH', WEB_ROOT . DS . 'templates' . DS . 'frontend' . DS . 'default' . DS);
+
+defined('BACKEND_TEMPLATES_PATH')
+|| define('BACKEND_TEMPLATES_PATH', WEB_ROOT . DS . 'templates' . DS . 'bo' . DS . 'default' . DS);
+
+defined('ASSETS_PATH')
+|| define('ASSETS_PATH', WEB_ROOT . DS . 'assets' . DS);
+
+defined('ASSETS_LINK')
+|| define('ASSETS_LINK', 'http://assets.projectbase.local');
+
